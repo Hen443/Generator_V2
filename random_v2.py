9bbd04d78d3bb156a10ccefcbe838f042d035246
Hen443 <44678813+Hen443@users.noreply.github.com>
2020-01-04 23:12:06 +0400

    Add files via upload

---
+++ random_v2.py
@@ -0,0 +1,20 @@
+import sys
+from random import randint
+
+if sys.argv[1] == "-nish" and sys.argv[3] == "-qanak":
+    i = int(sys.argv[2])
+    range_start = int(10**(i-1))
+    range_end = int((10**i)-1)
+    q = int(sys.argv[4])
+    if q == 0:
+        b = -1
+        while b < q:
+            print(str(randint(range_start, range_end)) + " ")
+    else:
+        b = 0
+        while b < q:
+            print(str(randint(range_start, range_end)) + " ")
+            b += 1
+
+else:
+    print("error")
